74f21433aea4611a40547c59302c2a6ca8da0768
daniel <morgenstern.daniel@gmail.com>
2014-04-25 01:43:25 +0200

    Neuer Befehl für Bash-Include-Listings

---
+++ code/cordova.sh
@@ -0,0 +1,24 @@
+#
+#
+#
+#
+#
+#
+#
+# 
+#
+############################################################################
+
+$ cordova create hello-beuth de.beuth-hochschule.hello HelloBeuth
+$ cordova platform add android
+$ cordova platform remove android
+$ cordova platforms list
+$ cordova build android
+$ cordova prepare android
+$ cordova compile android
+$ cordova emulate android
+$ cordova run android
+$ cordova plugin search contacts
+$ cordova plugin add org.apache.cordova.contacts
+$ cordova plugin ls
+$ cordova plugin rm org.apache.cordova.contacts
